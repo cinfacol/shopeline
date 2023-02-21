2de1a112c4dbf6b5c1dba7821df00c2e790185c6
jorge <cinfacol@gmail.com>
2023-02-21 12:32:23 -0500

    setup nginx

--- backend/tests/factories.py
+++ backend/tests/factories.py
@@ -10,7 +10,7 @@ faker = FakerFactory.create()
 @factory.django.mute_signals(post_save)
 class ProfileFactory(factory.django.DjangoModelFactory):
     user = factory.SubFactory("tests.factories.UserFactory")
-    phone_number = factory.LazyAttribute(lambda x: faker.phone_number())
+    # phone_number = factory.LazyAttribute(lambda x: faker.phone_number())
     about_me = factory.LazyAttribute(lambda x: faker.sentence(nb_words=5))
     license = factory.LazyAttribute(lambda x: faker.text(max_nb_chars=6))
     profile_photo = factory.LazyAttribute(
